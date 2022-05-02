496b31fb4cdf4b3e91f88bd2cf11c928b28c2757
BSandfer <bsandfer@gmail.com>
2022-05-02 01:13:06 -0700

    secrets are safe with me email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { FormControl, FormHelperText, Grid, Input, InputLabel } from '@mui/material'
 
 const Contact = () => {
   return (
     <>
     <Grid container justifyContent="center">
-        <Grid item>
-            <h1>Contact</h1>
+      <Grid item>
+        <h1>Contact</h1>
+      </Grid>
+    </Grid>
+    <Grid container justifyContent="center">
+        <Grid item xs={12} md={8} lg={7}>
+        <FormControl fullWidth>
+          <InputLabel variant="outlined" htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">your secrets are safe with me. || send me an email.</FormHelperText>
+        </FormControl>
         </Grid>
     </Grid>
     </>
