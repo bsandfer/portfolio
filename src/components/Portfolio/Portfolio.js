7f0b3f241843e38e812a27e5e65de093ad044ffe
BSandfer <bsandfer@gmail.com>
2022-05-01 18:57:47 -0700

    card work

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,45 +1,46 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import Card from "../Card";
 
 const Portfolio = () => {
   let projects = [
     {
-      title: "example",
+      title: "example1",
       github: "https://github.com/",
       heroku: "https://dashboard.heroku.com/",
       image:
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
     },
     {
-      title: "example",
+      title: "example2",
       github: "https://github.com/",
       heroku: "https://dashboard.heroku.com/",
       image:
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
     },
     {
-      title: "example",
+      title: "example3",
       github: "https://github.com/",
       heroku: "https://dashboard.heroku.com/",
       image:
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
     },
     {
-      title: "example",
+      title: "example4",
       github: "https://github.com/",
       heroku: "https://dashboard.heroku.com/",
       image:
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
     },
     {
-      title: "example",
+      title: "example5",
       github: "https://github.com/",
       heroku: "https://dashboard.heroku.com/",
       image:
         "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
     },
     {
-      title: "example",
+      title: "example6",
       github: "https://github.com/",
       heroku: "https://dashboard.heroku.com/",
       image:
@@ -49,11 +50,21 @@ const Portfolio = () => {
 
   return (
     <>
+
       <Grid container justifyContent="center">
-        <Grid item>
           <h1>My Portfolio</h1>
-        </Grid>
       </Grid>
+
+      <Grid container spacing={4} padding={2} justifyContent="center">
+
+          {projects.map(project =>
+                <Grid item>
+                    <Card title={project.title} github={project.github} heroku={project.heroku} image={project.image}></Card>
+                </Grid>
+            )}
+
+      </Grid>
+
     </>
   );
 };
