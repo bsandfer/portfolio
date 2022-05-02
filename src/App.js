f0ff0109fe227b3c981ff79376631e8b17253aa1
BSandfer <bsandfer@gmail.com>
2022-05-01 17:03:41 -0700

    about init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Navbar from "./components/Navbar"
+import CssBaseline from '@mui/material/CssBaseline';
+import About from "./components/About";
 
 function App() {
   return (
     <>
-    Hello world!
+    <CssBaseline/>
+    <Navbar/>
+    <About/>
     </>
   );
 }
