289794b78c8bf8e573f49672b10f158a62ad13ba
BSandfer <bsandfer@gmail.com>
2022-05-04 09:57:45 -0700

    theme added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,44 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#423E37',
+    },
+    secondary: {
+      main: '#E3B23C',
+      light: '#f5bd6b',
+    },
+    background: {
+      default: '#EDEBD7',
+      paper: '#fafafa',
+    },
+    error: {
+      main: '#c3474c',
+    },
+    info: {
+      main: '#ba68c8',
+    },
+  }
+})
+
 
 
 function App() {
   return (
     <>
-    <CssBaseline/>
-    <Navbar/>
-    <About/>
-    <Portfolio/>
-    <Contact/>
-    <Footer/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Navbar/>
+        <About/>
+        <Portfolio/>
+        <Contact/>
+        <Footer/>
+      </ThemeProvider>
     </>
   );
 }
