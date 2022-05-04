f7e0a183a9ee6207b9b7dc2b6e5535978b5c6d95
BSandfer <bsandfer@gmail.com>
2022-05-04 12:26:42 -0700

    styles .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,26 @@
 import { Grid, IconButton } from '@mui/material'
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import React from 'react'
 
 
 const Footer = () => {
   return (
     <>
-        <Grid container justifyContent="center" spacing={2} padding={2}>
+        <Grid container justifyContent="center" spacing={1} padding={1}>
             <Grid item>
-                <IconButton>
-                  <a href='https://github.com/bsandfer' target="_blank" rel="noreferrer">
+               
+                  <IconButton href='https://github.com/bsandfer' target="_blank" rel="noreferrer">
                     <GitHubIcon fontSize='large' color="primary"></GitHubIcon>
-                  </a>
-                </IconButton>
+                  </IconButton>
+                
+            </Grid>
+            <Grid item>
+                
+                  <IconButton href='https://www.linkedin.com/' target="_blank" rel="noreferrer">
+                    <LinkedInIcon fontSize='large' color="primary"></LinkedInIcon>
+                  </IconButton>
+               
             </Grid>
         </Grid>
     </>
