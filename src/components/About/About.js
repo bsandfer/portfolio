2dbe368f59ed6d81ead5fa275fceda6999aa7b63
BSandfer <bsandfer@gmail.com>
2022-05-04 19:08:49 -0700

    avatar pita

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,19 +4,30 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import headshot from "../../assets/images/parkPup.png";
 import { Avatar, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+
+const SizedAvatar = styled(Avatar)`
+  ${({ size, theme }) => `
+    width: ${theme.spacing(size)}px; 
+    height: ${theme.spacing(size)}px; 
+  `};
+`;
+    
+ 
 
 const About = () => {
   return (
     <>
       <Box marginTop={3}>
-          <Grid justifyContent="center" container padding={2} spacing={2}>
+          <Grid  sx={{mx: "auto"}}  justifyContent="center" container padding={2} spacing={2}>
             
-            <Grid item xs={12} md={5}>
-              <Avatar
+            <Grid item md={4}>
+              <SizedAvatar
                 alt="photo of me"
+                size="300"
                 src={headshot}
-                sx={{ width: 1, height: 1 }}
-              ></Avatar>
+                // sx={{ width: 1, height: 1 }}
+              ></SizedAvatar>
             </Grid>
 
             <Grid item md={1}></Grid>
@@ -29,9 +40,6 @@ const About = () => {
                   ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                   aliquip ex ea commodo consequat. 
               </Typography>
-            </Grid>
-
-            <Grid item xs={12} md={3}>
             </Grid>
 
           </Grid>
