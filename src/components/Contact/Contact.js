5e02169dfc6bcc9bbeed14d2d9e5e68a496a10d1
BSandfer <bsandfer@gmail.com>
2022-05-05 13:35:35 -0700

    temp email copy

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,79 +1,26 @@
-import { useState } from "react";
-import {
-  Box,
-  Button,
-  FormControl,
-  FormHelperText,
-  Grid,
-  Input,
-  InputLabel,
-  OutlinedInput,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material"
+import React, { Component } from "react"
 
-const Contact = () => {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-    nameNeeded: false,
-    messageNeeded: false,
-    invalidEmail: false,
-  });
+export default class YourComponent extends Component {
+  constructor(props) {
+    super(props)
 
-  const handleInputChange = ({ target: { name, value } }) => {
-    setFormState({ ...formState, [name]: value });
-  };
-
-  const handleNameBlur = () => {
-    if (formState.name === "") {
-      setFormState({
-        ...formState,
-        nameNeeded: true,
-        invalidEmail: false,
-        messageNeeded: false,
-      });
+    this.state = {
+      copySuccess: false
     }
-  };
+  }
 
-  const handleMessageBlur = () => {
-    if (formState.message === "") {
-      setFormState({
-        ...formState,
-        messageNeeded: true,
-        invalidEmail: false,
-        nameNeeded: false,
-      });
-    }
-  };
+  copyCodeToClipboard = () => {
+    const el = this.textArea
+    el.select()
+    document.execCommand("copy")
+    this.setState({copySuccess: true})
+  }
 
-  const validateEmail = (inputText) => {
-    var mailFormat =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (inputText.match(mailFormat)) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const handleEmailBlur = () => {
-    if (validateEmail(formState.email)) {
-      console.log("valid");
-    } else {
-      setFormState({
-        ...formState,
-        invalidEmail: true,
-        nameNeeded: false,
-        messageNeeded: false,
-      });
-    }
-  };
-
-  return (
-    <>
-      <Box marginTop={3}>
+  render() {
+    return (
+      <Box margin={3}>
+        
         <Grid container justifyContent="center">
           <Grid item>
             <Typography variant="h3">Contact</Typography>
@@ -83,81 +30,32 @@ const Contact = () => {
         <br></br>
 
         <Grid container justifyContent="center" spacing={3} padding={3}>
-          <Grid item xs={11} md={9} lg={8}>
-            <FormControl fullWidth>
-              <TextField
-                required
-                id="outlined-input"
-                label="Name"
-                type="text"
-                name="name"
-                autoComplete="name"
-                onChange={handleInputChange}
-                onBlur={handleNameBlur}
-              />
-            </FormControl>
+
+          <Grid item>
+              <div>
+                <div>
+                  <textarea
+                    ref={(textarea) => this.textArea = textarea}
+                    value="bSandfer@gmail.com"
+                  />
+                </div>
+                <div>
+                  <button onClick={() => this.copyCodeToClipboard()}>
+                    Copy to Clipboard
+                  </button>
+                  {
+                    this.state.copySuccess ?
+                    <div style={{"color": "green"}}>
+                      Success!
+                    </div> : null
+                  }
+                </div>
+              </div>
           </Grid>
 
-          <Grid item xs={11} md={9} lg={8}>
-            <FormControl fullWidth>
-              <TextField
-                required
-                id="outlined-input"
-                label="Email address"
-                type="email"
-                name="email"
-                autoComplete="email"
-                helperText="i won't share your email!"
-                onChange={handleInputChange}
-                onBlur={handleEmailBlur}
-              />
-            </FormControl>
-          </Grid>
-
-          <Grid item xs={11} md={9} lg={8}>
-            <FormControl fullWidth>
-              <TextField
-                required
-                id="outlined-input"
-                label="Your message:"
-                type="text"
-                name="message"
-                multiline
-                rows={3}
-                autoComplete="off"
-                onChange={handleInputChange}
-                onBlur={handleMessageBlur}
-              />
-            </FormControl>
-          </Grid>
-
-          <Grid container justifyContent="center" spacing={3} padding={3}>
-            <Grid item xs={11} md={9} lg={8}>
-              <Button variant="contained">Send</Button>
-            </Grid>
-          </Grid>
-
-          <Grid container justifyContent="center" spacing={3} padding={3}>
-            {formState.nameNeeded ? (
-              <Typography color="error">name required!</Typography>
-            ) : (
-              ""
-            )}
-            {formState.messageNeeded ? (
-              <Typography color="error">message required!</Typography>
-            ) : (
-              ""
-            )}
-            {formState.invalidEmail ? (
-              <Typography color="error">invalid email!</Typography>
-            ) : (
-              ""
-            )}
-          </Grid>
         </Grid>
-      </Box>
-    </>
-  );
-};
 
-export default Contact;
+      </Box>
+    )
+  }
+}
