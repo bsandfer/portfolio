0df3523f97ec2678e510de6eaa4f356fbd4bb0e0
BSandfer <bsandfer@gmail.com>
2022-05-10 16:41:54 -0700

    tooltips!

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import { Grid, IconButton, Paper } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from '@mui/icons-material/Email';
 import React from "react";
+import { Tooltip } from "@mui/material";
 
 const Footer = () => {
   return (
@@ -19,6 +21,7 @@ const Footer = () => {
           bgcolor="#a6a69b"
         >
           <Grid item>
+            <Tooltip title="Github">
             <IconButton
               href="https://github.com/bsandfer"
               target="_blank"
@@ -26,8 +29,10 @@ const Footer = () => {
             >
               <GitHubIcon fontSize="large" color="primary"></GitHubIcon>
             </IconButton>
+            </Tooltip>
           </Grid>
           <Grid item>
+            <Tooltip title="LinkedIn">
             <IconButton
               href="https://www.linkedin.com/"
               target="_blank"
@@ -35,6 +40,16 @@ const Footer = () => {
             >
               <LinkedInIcon fontSize="large" color="primary"></LinkedInIcon>
             </IconButton>
+            </Tooltip>
+          </Grid>
+          <Grid item>
+            <Tooltip title="send me an Email">
+            <IconButton
+              href="mailto:bsandfer@gmail.com"
+            >
+              <EmailIcon fontSize="large" color="primary"></EmailIcon>
+            </IconButton>
+            </Tooltip>
           </Grid>
         </Grid>
       </Paper>
